03e9118ff26e713e246a770cea4f83802e3d6306
bakelit69 <113536297+bakelit69@users.noreply.github.com>
2022-09-27 09:43:45 +0200

    Update Calculator.cs

--- Open-Lab-02.06/Calculator.cs
+++ Open-Lab-02.06/Calculator.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Open_Lab_02._06
 {
@@ -6,7 +6,14 @@ namespace Open_Lab_02._06
     {
         public bool Divisible(int number)
         {
-            throw new NotImplementedException();
+            if (number % 100 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
